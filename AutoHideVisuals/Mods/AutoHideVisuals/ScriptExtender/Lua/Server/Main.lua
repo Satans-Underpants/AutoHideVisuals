a556861e48c1d532f9ffb14f5a027d5214177170
Satans-Underpants <128394890+Satans-Underpants@users.noreply.github.com>
2024-09-12 10:46:34 +0200

    removed romance functionality (to be reworked)

--- AutoHideVisuals/Mods/AutoHideVisuals/ScriptExtender/Lua/Server/Main.lua
+++ AutoHideVisuals/Mods/AutoHideVisuals/ScriptExtender/Lua/Server/Main.lua
@@ -25,70 +25,70 @@ local queued = false
 --------------------------------------------------------------------------------------------------------
 
 -- Saves and removes the Visuals for a romance scene
-Ext.Osiris.RegisterListener("QRY_CampNight_TryStartSleepMoment_RomanceNight", 3, "before", function(DIALOGRESOURCE, CHARACTERA, CHARACTERB) 
+-- Ext.Osiris.RegisterListener("QRY_CampNight_TryStartSleepMoment_RomanceNight", 3, "before", function(DIALOGRESOURCE, CHARACTERA, CHARACTERB) 
 
 
-        -- partner 1
-        local currentVisualsA = Visuals:getVisuals(CHARACTERA)
-        UserVars:AssignVisuals(CHARACTERA, currentVisualsA)
-        Visuals:takeOffVisual(CHARACTERA, GLOBALDEBUG)
-        Visuals:removeVisualSetSlot(GLOBALDEBUG, CHARACTERA)
+--         -- partner 1
+--         local currentVisualsA = Visuals:getVisuals(CHARACTERA)
+--         UserVars:AssignVisuals(CHARACTERA, currentVisualsA)
+--         Visuals:takeOffVisual(CHARACTERA, GLOBALDEBUG)
+--         Visuals:removeVisualSetSlot(GLOBALDEBUG, CHARACTERA)
 
-        -- partner 2
+--         -- partner 2
 
-        local currentVisualsB = Visuals:getVisuals(CHARACTERB)
-        UserVars:AssignVisuals(CHARACTERB, currentVisualsB)
-        Visuals:takeOffVisual(CHARACTERB, GLOBALDEBUG)
-        Visuals:removeVisualSetSlot(GLOBALDEBUG, CHARACTERB)
+--         local currentVisualsB = Visuals:getVisuals(CHARACTERB)
+--         UserVars:AssignVisuals(CHARACTERB, currentVisualsB)
+--         Visuals:takeOffVisual(CHARACTERB, GLOBALDEBUG)
+--         Visuals:removeVisualSetSlot(GLOBALDEBUG, CHARACTERB)
 
-        romanceHasHappened = true
-end)
+--         romanceHasHappened = true
+-- end)
 
 
 
 -- Triggers when a Romance is over. Reapply Visual
-Ext.Osiris.RegisterListener("TeleportedToCamp", 1, "before", function(character)
+-- Ext.Osiris.RegisterListener("TeleportedToCamp", 1, "before", function(character)
 
-    if queued then
-        return
-    end
+--     if queued then
+--         return
+--     end
 
-    queued = true
+--     queued = true
 
-    if romanceHasHappened then
+--     if romanceHasHappened then
 
-        local party = Osi.DB_PartyMembers:Get(nil)
-        _D(party)
-        for i = #party, 1, -1 do
-           local character = party[i][1]
+--         local party = Osi.DB_PartyMembers:Get(nil)
+--         _D(party)
+--         for i = #party, 1, -1 do
+--            local character = party[i][1]
 
 
-            -- if autoVisual passive is activated, toggle it
-            if Osi.HasActiveStatus(character, "INSERT_YOUR_TAKE_OFF_STATUS_NAME_HERE") == 0 then
+--             -- if autoVisual passive is activated, toggle it
+--             if Osi.HasActiveStatus(character, "INSERT_YOUR_TAKE_OFF_STATUS_NAME_HERE") == 0 then
 
-                print("status not active for ", character)
+--                 print("status not active for ", character)
 
-                Osi.TogglePassive(character, "INSERT_YOUR_TAKE_OFF_PASSIVE_NAME_HERE")
-                Ext.Timer.WaitFor(300, function ()
-                    Osi.TogglePassive(character, "INSERT_YOUR_TAKE_OFF_PASSIVE_NAME_HERE")
-                end)
+--                 Osi.TogglePassive(character, "INSERT_YOUR_TAKE_OFF_PASSIVE_NAME_HERE")
+--                 Ext.Timer.WaitFor(300, function ()
+--                     Osi.TogglePassive(character, "INSERT_YOUR_TAKE_OFF_PASSIVE_NAME_HERE")
+--                 end)
             
-            end
-        end
+--             end
+--         end
 
 
-        Ext.Timer.WaitFor(15000, function ()
-            romanceHasHappened = false
-            queued = false
-        end)
+--         Ext.Timer.WaitFor(15000, function ()
+--             romanceHasHappened = false
+--             queued = false
+--         end)
         
 
-    end
+--     end
 
-end)
+-- end)
 
 
 
-Ext.Osiris.RegisterListener("WentOnStage", 2, "after", function(object, isOnStageNow)  end)
+-- Ext.Osiris.RegisterListener("WentOnStage", 2, "after", function(object, isOnStageNow)  end)
 
 
